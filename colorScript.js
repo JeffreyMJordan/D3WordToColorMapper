cce22c5abf8e83336e478af161f593306a68cf0f
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2018-02-24 16:05:19 -0800

    Completes wordToReal

--- colorScript.js
+++ colorScript.js
@@ -1,5 +1,11 @@
-window.addEventListener('DOMContentLoaded', () => {
-  console.log("Script included");
-
+document.addEventListener('DOMContentLoaded', () => {
   
+  let rainbow = d3.scaleSequential(d3.interpolateRainbow);
+  let form = document.getElementById("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let val = document.getElementById("text").value;
+    console.log(val);
+    console.log(rainbow(parseFloat(val)));
+  });
 });
