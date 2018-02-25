51f771462ff53476798099adb5e21277f03e939b
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2018-02-24 16:15:49 -0800

    Alters word to real to add more color diversity

--- colorScript.js
+++ colorScript.js
@@ -1,11 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  let alphabetHash = {};
+  for(let i = 0; i<alphabet.length; i++){
+    alphabetHash[alphabet[i]] = i.toString();
+  }
+
+  const wordToReal = (word) => {
+    word = word.toLowerCase();
+    let numStr = ""
+    for(let i = 0; i<word.length; i++){
+      numStr += alphabetHash[word[i]];
+    }
+    let divStr = "1";
+    for(let j = 0; j<numStr.length; j++){
+      divStr += "0"
+    }
+    return (parseFloat(numStr.split("").reverse().join(""))/parseFloat(divStr) );
+  };
+
   let rainbow = d3.scaleSequential(d3.interpolateRainbow);
   let form = document.getElementById("form");
+
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     let val = document.getElementById("text").value;
-    console.log(val);
-    console.log(rainbow(parseFloat(val)));
+    let num = wordToReal(val);
+    let colorNode = document.getElementById("color");  
+    console.log(num);
+    console.log(rainbow(num));
+    colorNode.style.color = rainbow(num);
   });
 });
